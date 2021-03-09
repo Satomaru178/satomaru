a8a35cd48ce6477d5571a152618772b607e70005
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-09 21:54:27 +0900

    lesson 68 shortcode

--- functions.php
+++ functions.php
@@ -6,6 +6,11 @@
  *
  * @package satomaru
  */
+add_shortcode('date', function() {
+	return get_the_title();
+	return date('Y年 n月 j日');
+});
+
 add_action('init', function() {
 	register_post_type( 'item', [
 		'label' => '商品',
